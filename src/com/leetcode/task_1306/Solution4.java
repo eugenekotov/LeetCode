089d52673a697f39dceeccf1be0a8d6f2b24eda8
eugenekotov <eugene1501@mail.ru>
2020-11-29 14:24:53 +0200

    1306 other ways

--- src/com/leetcode/task_1306/Solution4.java
+++ src/com/leetcode/task_1306/Solution4.java
@@ -1,19 +1,19 @@
 package com.leetcode.task_1306;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Solution {
+public class Solution4 {
 
     /**
      * BFS Поиск в ширину
      */
     public boolean canReach(int[] arr, int start) {
         boolean[] visited = new boolean[arr.length];
-        Queue<Integer> indexes = new LinkedList<>();
-        indexes.offer(start);
+        List<Integer> indexes = new ArrayList<>();
+        indexes.add(start);
         while (!indexes.isEmpty()) {
-            int index = indexes.remove();
+            int index = indexes.remove(0);
             if (0 <= index && index < arr.length && !visited[index]) {
                 if (arr[index] == 0) {
                     return true;
